909728ce8eae1e84796714c9887d2a31b44c363e
Andres Hernandez <edanhebla1213@hotmail.com>
2023-03-15 22:21:25 -0500

    combos link components ts

--- frontend/src/app/pages/components/combos-link/combos-link.component.ts
+++ frontend/src/app/pages/components/combos-link/combos-link.component.ts
@@ -1,25 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CombosService } from '../../../services/combos.service';
 
-
 @Component({
   selector: 'app-combos-link',
   templateUrl: './combos-link.component.html',
-  styleUrls: ['./combos-link.component.css']
+  styleUrls: ['./combos-link.component.css'],
 })
 export class CombosLinkComponent implements OnInit {
-
-  routesLinkCombos: any = []
-  constructor(private combosService: CombosService) { }
+  routesLinkCombos: any = [];
+  constructor(private combosService: CombosService) {}
 
   ngOnInit(): void {
-    this.combosService.getCombos().subscribe(({combos} : any) => {
-      combos.forEach((combo:any) => {
-        const route = `/combo/${combo.title}`
-        this.routesLinkCombos.push(route)
-      })
+    this.combosService.getCombos().subscribe(({ combos }: any) => {
+      combos.forEach((combo: any) => {
+        const route = `/combo/${combo.pageUrl}`;
+        this.routesLinkCombos.push(route);
+      });
     });
   }
-
 }
-
